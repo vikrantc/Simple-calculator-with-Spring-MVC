c43326d7bf418a994021472539d85075d1446076
vikrant chauhan <meetvikrant@gmail.com>
2020-12-29 11:19:14 +0530

    Modified application to start at specific port

--- src/main/java/com/softuni/CalculatorApplication.java
+++ src/main/java/com/softuni/CalculatorApplication.java
@@ -3,9 +3,14 @@ package com.softuni;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class CalculatorApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(CalculatorApplication.class, args);
+		SpringApplication app = new SpringApplication(CalculatorApplication.class);
+		app.setDefaultProperties(Collections
+				.singletonMap("server.port", "9000"));
+		app.run(args);
 	}
 }

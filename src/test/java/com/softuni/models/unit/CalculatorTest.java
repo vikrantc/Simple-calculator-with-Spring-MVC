1f8f9af12f7c2eae3bbffc0dd44e01474eb84660
vikrant chauhan <meetvikrant@gmail.com>
2021-01-06 15:56:46 +0530

    Used testng instead of Junit

--- src/test/java/com/softuni/models/unit/CalculatorTest.java
+++ src/test/java/com/softuni/models/unit/CalculatorTest.java
@@ -1,47 +1,53 @@
 package com.softuni.models.unit;
 
 import com.softuni.models.Calculator;
-import junit.framework.TestCase;
+import org.testng.annotations.Test;
 
-public class CalculatorTest extends TestCase {
+import static org.testng.AssertJUnit.assertEquals;
 
+public class CalculatorTest {
 
+    @Test
     public void testGetLeftOperand() {
         Calculator calculator = new Calculator(1,2,"+");
         assertEquals(1.0,calculator.getLeftOperand());
 
     }
 
-
+    @Test
     public void testSetLeftOperand() {
         Calculator calculator = new Calculator(1,2,"+");
         calculator.setLeftOperand(2);
         assertEquals(2.0,calculator.getLeftOperand());
     }
 
+    @Test
     public void testGetRightOperand() {
         Calculator calculator = new Calculator(1,2,"+");
         assertEquals(2.0,calculator.getRightOperand());
     }
 
+    @Test
     public void testSetRightOperand() {
         Calculator calculator = new Calculator(1,2,"+");
         calculator.setRightOperand(1);
         assertEquals(1.0,calculator.getRightOperand());
     }
 
-
+    @Test
     public void testGetOperator() {
         Calculator calculator = new Calculator(1,2,"+");
         assertEquals("+",calculator.getOperator());
     }
 
+    @Test
     public void testSetOperator() {
         Calculator calculator = new Calculator(1,2,"+");
         calculator.setOperator("-");
         assertEquals("-",calculator.getOperator());
     }
 
+    @Test
     public void testCalculateResult() {
         Calculator calculator = new Calculator(1,2,"+");
         assertEquals(3.0,calculator.calculateResult());
